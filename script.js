da86d24046803812dbbb298dea42a6e878967e68
ajgoras <ajgoras@MacBook-Pro-ajgoras.local>
2022-01-02 13:01:00 +0100

    added allwaterdiv, allWaterLs, increasing allWater value, allWater style

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ var container2Count = document.querySelector('#container2Val');
 var container2CounterLs = localStorage.getItem('container2Count');
 var container3Count = document.querySelector('#container3Val');
 var container3CounterLs = localStorage.getItem('container3Count');
+var allWater = document.querySelector('#allWater');
+var allWaterLs = localStorage.getItem('allWater');
 
 if (container1CounterLs!=null) {
     container1Count.innerHTML = parseInt(container1CounterLs);
@@ -17,6 +19,9 @@ if (container2CounterLs != null) {
 }
 if (container3CounterLs != null) {
     container3Count.innerHTML = parseInt(container3CounterLs);
+}
+if (allWaterLs != null) {
+    allWater.innerHTML = parseFloat(allWaterLs);
 }
 
 var containerValues = document.querySelectorAll('.containerValues');
@@ -29,6 +34,7 @@ containerDivs[2].querySelector('h2').innerHTML = bigBottle.name + ' (' + bigBott
 container1Button.addEventListener('click', () => {
     container1CounterLs = localStorage.getItem('container1Count');
     var counter = 0;
+    var containerCapacity = parseFloat(allWater.innerHTML);
     if (container1CounterLs==null) {
         counter = 0;
     }
@@ -38,6 +44,9 @@ container1Button.addEventListener('click', () => {
     counter = counter + 1;
     container1Count.innerHTML = counter;
     localStorage.setItem('container1Count', counter);
+    containerCapacity += GlassOfWater.capacity/1000;
+    allWater.innerHTML = containerCapacity;
+    localStorage.setItem('allWater', containerCapacity);
     container1Button.classList.add('bounce-4');
     setTimeout(() => {
         container1Button.classList.remove('bounce-4');
@@ -86,6 +95,7 @@ resetButton.addEventListener('click', () => {
     counts.forEach(element => {
         element.innerHTML = 0;
     });
+    allWater.innerHTML = 0;
     resetButton.classList.add('bounce-4');
     setTimeout(() => {
         resetButton.classList.remove('bounce-4');
