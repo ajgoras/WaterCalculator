39b752a612ea8dc9790bddcd98c177fe2f2e4921
ajgoras <ajgoras@MacBook-Pro-ajgoras.local>
2022-01-25 15:56:35 +0100

    improved reveal animation

--- revealing.js
+++ revealing.js
@@ -22,5 +22,19 @@ function revealtime() {
         elements[i].classList.add('delayed');
     }
 }
+function revealtime2() {
+    var elements = document.getElementsByClassName('revealtime2');
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.add('delayed');
+    }
+}
+function revealtime3() {
+    var elements = document.getElementsByClassName('revealtime3');
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.add('delayed');
+    }
+}
 
 setTimeout(() => { revealtime(); }, 600);
+setTimeout(() => { revealtime2(); }, 800);
+setTimeout(() => { revealtime3(); }, 1050);
